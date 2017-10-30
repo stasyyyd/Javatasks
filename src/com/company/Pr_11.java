5c36d27137357646c3d9fde2b8ec36e4e2c5d359
Dashutina_AV <nassty1919@gmail.com>
2017-10-30 17:03:18 +1000

    добавлены задачи 11 и 1

---
+++ src/com/company/Pr_11.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Pr_11 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+        double dlina=Integer.parseInt(r.readLine());
+        double pi=3.14;
+        double radius = dlina*pi;
+        System.out.println("Диаметр окружности = :" + " " +radius);
+
+    }
+}
