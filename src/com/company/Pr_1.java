a25118d0bb7851048000c1437e4f66e575181a2a
Dashutina_AV <nassty1919@gmail.com>
2017-10-30 17:26:05 +1000

    ``

--- src/com/company/Pr_1.java
+++ src/com/company/Pr_1.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Programm1 {
+public class Pr_1 {
     public static void main(String[] args) throws IOException{
         BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
         int natNum = Integer.parseInt(r.readLine());
