b2e76b82830d21b7188389c5c72202b9772e3925
Dashutina_AV <nassty1919@gmail.com>
2017-10-30 17:14:25 +1000

    добавлена задача 2

---
+++ src/com/company/Pr_2.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Created by Anastasia on 27.10.2017.
+ */
+public class Pr_2 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(r.readLine());
+        int num2=((num/10)%10);
+
+        System.out.println(num2);
+    }
+}
