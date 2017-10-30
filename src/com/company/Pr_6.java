273d8123bba1a747763bf391c465d9e8d3cb55ac
Dashutina_AV <nassty1919@gmail.com>
2017-10-30 17:21:29 +1000

    добавлена задача 6

---
+++ src/com/company/Pr_6.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Pr_6 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+        int sum=1;
+        System.out.println("Введите число = ");
+        int num=Integer.parseInt(r.readLine());
+        System.out.println("Введите степень = ");
+        double s=Integer.parseInt(r.readLine());
+        for (int i=0;i<s;i++){
+            sum=sum*num;
+        }
+        if (s<0){
+            System.out.println("Число в степени равно = " + Math.pow(num,s));
+        }else
+
+            System.out.println("Число в степени равно = " + sum);
+    }
+
+}
