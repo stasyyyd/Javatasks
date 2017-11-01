9e1b7f14191aa98482965ae4460e514df1814174
Dashutina_AV <nassty1919@gmail.com>
2017-11-01 17:48:01 +1000

    /

--- src/dvfu/java/tasks3/pr18.java
+++ src/dvfu/java/tasks3/pr18.java
@@ -6,12 +6,16 @@ import java.io.InputStreamReader;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.Date;
+import java.util.*;
+import java.text.*;
 
 public class pr18 {
     public static void main(String args[]) {
         // Инициализация объекта date
         Date date = new Date();
+        SimpleDateFormat formatForDateNow = new SimpleDateFormat("E yyyy.MM.dd 'и время' hh:mm:ss a zzz");
 
+        System.out.println("Текущая дата " + formatForDateNow.format(date));
         // Вывод текущей даты и времени с использованием toString()
         System.out.println(date.toString());
     }
