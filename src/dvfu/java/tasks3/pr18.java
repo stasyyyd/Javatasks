765cb4a8e41b560cf431ade2f9684e4feee05e87
Dashutina_AV <nassty1919@gmail.com>
2017-11-01 17:45:57 +1000

    добавлены задачи 1

--- src/dvfu/java/tasks3/pr18.java
+++ src/dvfu/java/tasks3/pr18.java
@@ -1,4 +1,18 @@
 package dvfu.java.tasks3;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Date;
+
 public class pr18 {
+    public static void main(String args[]) {
+        // Инициализация объекта date
+        Date date = new Date();
+
+        // Вывод текущей даты и времени с использованием toString()
+        System.out.println(date.toString());
+    }
 }
