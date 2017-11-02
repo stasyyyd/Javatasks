02beb2d9ccae2d084ace194068bc6d071aa3998a
Dashutina_AV <nassty1919@gmail.com>
2017-11-02 11:27:15 +1000

    решена задача 18

--- src/dvfu/java/tasks3/pr18.java
+++ src/dvfu/java/tasks3/pr18.java
@@ -6,17 +6,21 @@ import java.io.InputStreamReader;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.Date;
-import java.util.*;
 import java.text.*;
 
-public class pr18 {
-    public static void main(String args[]) {
-        // Инициализация объекта date
-        Date date = new Date();
-        SimpleDateFormat formatForDateNow = new SimpleDateFormat("E yyyy.MM.dd 'и время' hh:mm:ss a zzz");
+/*Какая дата будет через N дней? Рекомендуется
+использование типа GregorianCalendar.*/
 
-        System.out.println("Текущая дата " + formatForDateNow.format(date));
-        // Вывод текущей даты и времени с использованием toString()
-        System.out.println(date.toString());
+public class pr18 {
+    public static void main(String args[]) throws IOException {
+      BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+      System.out.print("Ввведите количество дней ");
+      int day=Integer.parseInt(r.readLine());// Инициализация объекта date
+
+        Calendar c = new GregorianCalendar();
+        c.add(Calendar.DAY_OF_MONTH, +day);
+        SimpleDateFormat formatDate = new SimpleDateFormat("E dd.MM.yyyy г.");
+        Date date = c.getTime();
+        System.out.println("Через " + day + " дней будет "+formatDate.format(date));
     }
 }
