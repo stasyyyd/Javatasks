6caea517be1ee72ed36c41793090545355902a4d
stasyyyd <nassty1919@gmail.com>
2017-12-08 16:08:26 +1000

    исправлены классы main,options,questions.

--- src/dvfu/java/testsystem/Test.java
+++ src/dvfu/java/testsystem/Test.java
@@ -1,12 +1,28 @@
 package dvfu.java.testsystem;
 
-import java.io.BufferedReader;
+
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
+
+
 
 public class Test implements TestSystem {
-    String nameTest;
+    public static void main(String[] args) throws IOException{
+
+        Test test=new Test();
+        test.show();
+
+        Options options=new Options();
+        options.getOptionQuestions();
+        options.getOptionQuestions1();
+        options.getOptionQuestions2();
+        options.getOptionQuestions3();
+        options.getOptionQuestions4();
+
+
+        System.out.println("Правильных ответов "+options.correctAnswer+" из 5");
+
+        
+    }
 
 
     @Override
